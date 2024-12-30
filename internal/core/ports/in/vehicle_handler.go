7364e64b4ed2573dc81e19e7aa2f6479d5f78f27
arianeASA <ariane_1959@hotmail.com>
2024-12-30 18:54:46 -0300

    unit testes

--- internal/core/ports/in/vehicle_handler.go
+++ internal/core/ports/in/vehicle_handler.go
@@ -1,7 +1,6 @@
 package in
 
 import (
-	"github.com/stretchr/testify/mock"
 	"net/http"
 )
 
@@ -10,25 +9,4 @@ type VehicleHandler interface {
 	HandleUpdateVehicle(w http.ResponseWriter, r *http.Request)
 	HandleListVehicles(w http.ResponseWriter, r *http.Request)
 	HandleSearchVehicleByID(w http.ResponseWriter, r *http.Request)
-}
-
-// MockVehicleHandler is a mock of VehicleHandler
-type MockVehicleHandler struct {
-	mock.Mock
-}
-
-func (m *MockVehicleHandler) HandleCreateVehicle(w http.ResponseWriter, r *http.Request) {
-	m.Called(w, r)
-}
-
-func (m *MockVehicleHandler) HandleUpdateVehicle(w http.ResponseWriter, r *http.Request) {
-	m.Called(w, r)
-}
-
-func (m *MockVehicleHandler) HandleListVehicles(w http.ResponseWriter, r *http.Request) {
-	m.Called(w, r)
-}
-
-func (m *MockVehicleHandler) HandleSearchVehicleByID(w http.ResponseWriter, r *http.Request) {
-	m.Called(w, r)
 }
