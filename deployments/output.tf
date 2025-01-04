7332ca8bc0e6d6bde8188fe167262215017cd30d
arianeASA <ariane_1959@hotmail.com>
2025-01-04 04:26:32 -0300

    test

--- deployments/output.tf
+++ deployments/output.tf
@@ -3,3 +3,18 @@ output "rds_endpoint" {
   value = aws_db_instance.postgres_rds.address
   description = "RDS Endpoint"
 }
+
+output "rds_username" {
+  value = aws_db_instance.postgres_rds.username
+    description = "RDS Username"
+}
+
+output "rds_password" {
+  value = aws_db_instance.postgres_rds.password
+    description = "RDS Password"
+}
+
+output "rds_db_name" {
+  value = var.db_name
+    description = "RDS Database Name"
+}
