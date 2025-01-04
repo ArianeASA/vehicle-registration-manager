9c00084cc6edb9593aa1e0502eb58cb831f9b323
arianeASA <ariane_1959@hotmail.com>
2025-01-04 04:28:08 -0300

    test

--- deployments/output.tf
+++ deployments/output.tf
@@ -7,14 +7,17 @@ output "rds_endpoint" {
 output "rds_username" {
   value = aws_db_instance.postgres_rds.username
     description = "RDS Username"
+  sensitive = true
 }
 
 output "rds_password" {
   value = aws_db_instance.postgres_rds.password
     description = "RDS Password"
+  sensitive = true
 }
 
 output "rds_db_name" {
   value = var.db_name
     description = "RDS Database Name"
+  sensitive = true
 }
