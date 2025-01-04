32eb8fdbd7381a8362b822e7140f9d13ac542705
arianeASA <ariane_1959@hotmail.com>
2025-01-04 04:58:56 -0300

    test

--- deployments/output.tf
+++ deployments/output.tf
@@ -1,18 +1,4 @@
-
 output "rds_endpoint" {
   value = aws_db_instance.postgres_rds.address
   description = "RDS Endpoint"
-}
-
-output "rds_username" {
-  value = aws_db_instance.postgres_rds.username
-    description = "RDS Username"
-  sensitive = false
-}
-
-
-output "rds_db_name" {
-  value = var.db_name
-    description = "RDS Database Name"
-  sensitive = false
 }
