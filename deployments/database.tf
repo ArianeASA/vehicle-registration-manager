38f4c7663f4d16bc217112bf572cb6226ca33e56
arianeASA <ariane_1959@hotmail.com>
2025-01-04 04:39:45 -0300

    test

--- deployments/database.tf
+++ deployments/database.tf
@@ -6,6 +6,7 @@ resource "aws_db_instance" "postgres_rds" {
   identifier           = var.db_name
   username             = var.db_username
   password             = var.db_password
+  db_name = var.db_name
   skip_final_snapshot = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
