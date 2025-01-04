2ce669add0bb5dff284580040e191cf039a34752
arianeASA <ariane_1959@hotmail.com>
2025-01-04 02:21:52 -0300

    cr sonar

--- deployments/database.tf
+++ deployments/database.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "postgres_rds" {
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
   publicly_accessible = true
-  backup_retention_period = 1
+  backup_retention_period = 2
   storage_encrypted = true
 }
 
