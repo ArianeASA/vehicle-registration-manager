65b7b9a57abf6b0487224dea35b40c31cf65fc7d
arianeASA <ariane_1959@hotmail.com>
2025-01-05 13:30:50 -0300

    test json

--- .github/workflows/extract_outputs.py
+++ .github/workflows/extract_outputs.py
@@ -1,8 +1,10 @@
 import sys
 import json
+import os
 
 # Obter o nome do arquivo dos argumentos de linha de comando
 state_file = sys.argv[1] if len(sys.argv) > 1 else 'default.tfstate'
+output_name = sys.argv[2] if len(sys.argv) > 2 else 'default_output_name'
 
 # Carregar o arquivo de estado do Terraform
 with open(state_file, 'r') as file:
@@ -14,3 +16,12 @@ outputs = state.get('outputs', {})
 # Imprimir os outputs
 for key, value in outputs.items():
     print(f"{key}: {value['value']}")
+
+# Extrair o valor do output desejado
+desired_output = outputs.get('your_output_name', {}).get('value', '')
+
+# Escrever o valor no arquivo GITHUB_ENV
+env_file = os.getenv('GITHUB_ENV')
+with open(env_file, "a") as myfile:
+    myfile.write(f"{output_name.upper()}={desired_output}\n")
+
