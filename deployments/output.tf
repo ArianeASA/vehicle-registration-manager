bcb41de0169f87a2cca7ee9ecc7e12c36aa20bdd
arianeASA <ariane_1959@hotmail.com>
2025-01-04 22:52:43 -0300

    test AWS_SECURITY_GROUP_ID

--- deployments/output.tf
+++ deployments/output.tf
@@ -2,3 +2,8 @@ output "rds_endpoint" {
   value = aws_db_instance.postgres_rds.address
   description = "RDS Endpoint"
 }
+
+output "rds_security_group_id" {
+  value = aws_security_group.rds_sg.id
+  description = "RDS Security Group ID"
+}
